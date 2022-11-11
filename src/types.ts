d357f345e8ca07221f52b2d0d003125e60d16f46
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-11-11 20:46:23 -0300

    typepadrao

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+  
+ export type Pessoa={
+    id : string,
+    name:string,
+    phone:number,
+    email: string,
+    website:string
+ }
+
+ export type User={
+   userId : number,
+   id:number,
+   title:string,
+   body: string
+  
+}
+
+ 
